316414bcbb4623065ed33e48ffb3e0095d4ebd25
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 23:58:59 +0300

    add loginand logout redirect

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from .  import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,4 +10,15 @@ urlpatterns = [
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('all-hoods/', views.hoods, name='hood'),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
+
 ]
--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -144,3 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'index'
