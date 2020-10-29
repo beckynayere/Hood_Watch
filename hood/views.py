1ee15cc3b1d15637c043c7fc5dfa798f9780ef49
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 23:33:46 +0300

    create a searchbusiness function

--- hood/views.py
+++ hood/views.py
@@ -115,3 +115,17 @@ def edit_profile(request, username):
     return render(request, 'edit_profile.html', {'form': form})
 
 
+def search_business(request):
+    if request.method == 'GET':
+        name = request.GET.get("title")
+        results = Business.objects.filter(name__icontains=name).all()
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, "results.html")
