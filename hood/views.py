d26d808d3878dd78a79633e42373c97bda96a439
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 23:32:52 +0300

    create a profile edit function

--- hood/views.py
+++ hood/views.py
@@ -99,3 +99,19 @@ def leave_hood(request, id):
     request.user.profile.save()
     return redirect('hood')
 
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile', user.username)
+    else:
+        form = UpdateProfileForm(instance=request.user.profile)
+    return render(request, 'edit_profile.html', {'form': form})
+
+
