e6010c5acf7f370e96024d20cb60b422b48ec6e3
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 23:29:57 +0300

    create a post def

--- hood/views.py
+++ hood/views.py
@@ -67,3 +67,18 @@ def single_hood(request, hood_id):
     }
     return render(request, 'single_hood.html', params)
 
+def create_post(request, hood_id):
+    hood = NeighbourHood.objects.get(id=hood_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('single-hood', hood.id)
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {'form': form})
+
+
