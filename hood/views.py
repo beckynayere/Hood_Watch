c06029085ba06726f7ebb3fc7d12031af45206c4
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 20:26:03 +0300

    add a signup view

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
