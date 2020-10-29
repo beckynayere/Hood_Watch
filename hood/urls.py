2a6ccf5d2d9b4b099d5b48aa05937a1d21b50961
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 19:58:24 +0300

    add urls pattern

---
+++ hood/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from .  import views
+
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+]
