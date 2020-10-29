dd150754519dcc71998b4eac09678f5811b0c457
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 22:52:51 +0300

    dreate a db

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='login')
