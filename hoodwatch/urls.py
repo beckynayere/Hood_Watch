bd2ef5b6f973fe307ed441f07af9ca0248e76c7a
becky nayere <rebecca.nayere@gmail.com>
2020-10-29 20:22:55 +0300

    create template

--- hoodwatch/urls.py
+++ hoodwatch/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    
+    url('', include ('hood.urls'))
 ]
