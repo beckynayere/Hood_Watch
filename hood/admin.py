f5934212b1a69b56340d76d760128dc7514ed148
becky nayere <rebecca.nayere@gmail.com>
2020-11-02 19:35:59 +0300

    register models

--- hood/admin.py
+++ hood/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, NeighbourHood, Business, Post
+from .models import Profile, NeighbourHood, Business, Post,Comment,Location,Category
 
 # Register your models here.
 
@@ -8,3 +8,6 @@ admin.site.register(Profile)
 admin.site.register(NeighbourHood)
 admin.site.register(Business)
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Location)
+admin.site.register(Category)
